72f2907786b7a8834ac20f6bdaa1f31d8bc8adc1
edlb-sneakernet <edlb-sneakernet@scicomp>
2019-06-18 12:33:37 -0400

    compliant for HiSeq runs now

--- scripts/SneakerNet.roRun.pl
+++ scripts/SneakerNet.roRun.pl
@@ -22,7 +22,7 @@ exit(main());
 
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help tempdir=s numcpus=i outdir=s)) or die $!;
+  GetOptions($settings,qw(help createsamplesheet tempdir=s numcpus=i outdir=s)) or die $!;
   $$settings{tempdir}||=tempdir("$0.XXXXXX",TMPDIR=>1,CLEANUP=>1);
   mkdir($$settings{tempdir}) if(!-e $$settings{tempdir});
   $$settings{numcpus}||=1;
@@ -58,14 +58,18 @@ sub makeSneakernetDir{
   
   if(! -e $sampleSheet){
     if(! -e $sampleSheet2){
-      die "ERROR: could not find the samplesheet in either $sampleSheet or $sampleSheet2";
+      if($$settings{createsamplesheet}){
+        # do nothing
+      } else {
+        die "ERROR: could not find the samplesheet in either $sampleSheet or $sampleSheet2";
+      }
     }
     $sampleSheet = $sampleSheet2;
   }
   if(!@interop){
     @interop  = glob("$dir/QC/InterOp/*");
     if(!@interop){
-      die "ERROR: no interop files were found in $dir";
+      logmsg "ERROR: no interop files were found in $dir";
     }
   }
   #for(@fastq, $config, @interop, @xml){
@@ -73,6 +77,11 @@ sub makeSneakernetDir{
   #    die "ERROR: file does not exist: $_";
   #  }
   #}
+
+  if($$settings{createsamplesheet}){
+    $sampleSheet = createSampleSheet($dir, $outdir, $settings);
+  }
+
   if(-e $snok){
     cp($snok,"$outdir/".basename($snok));
   } else {
@@ -83,7 +92,7 @@ sub makeSneakernetDir{
   }
   if(!@fastq){
     # If there aren't any fastq files, try to see if they're in the main dir
-    @fastq = glob("$dir/*.fastq.gz");
+    @fastq = glob("$dir/*.fastq.gz $dir/*/*.fastq.gz");
     if(!@fastq){
       logmsg "WARNING: no fastq files were found; attempting bcl2fastq";
       @fastq=bcl2fastq($dir,$settings);
@@ -138,11 +147,48 @@ sub cp{
     return 1;
   }
   logmsg "cp $from to $to";
-  my $return=File::Copy::cp($from,$to) or warn "ERROR: could not copy $from to $to: $!";
+  my $return = link($from, $to) ||
+    File::Copy::cp($from,$to) or warn "ERROR: could not copy $from to $to: $!";
   open(my $fh, ">>", $to) or die "ERROR: could not write to $to: $!";
   close $fh;
   return $return;
 }
+
+sub createSampleSheet{
+  my($dir, $outdir, $settings) = @_;
+
+  my $samplesheet = "$outdir/SampleSheet.csv";
+  if(-e $samplesheet){
+    die "ERROR: was going to create a samplesheet but it already exists at $samplesheet";
+  }
+  open(my $fh, ">", $samplesheet) or die "ERROR: could not write to $samplesheet: $!";
+  print $fh "[Data]\n";
+  for my $demuxSamples(glob("$dir/*_*.csv")){ # should just be one file
+    open(my $demuxFh, "<", $demuxSamples) or die "ERROR: could not read from $demuxSamples: $!";
+    my $found_the_samples = 0;
+    while(<$demuxFh>){
+      if(/,Sample,/){
+        s/,Sample,/,SampleID,/;
+      }
+      if(/Flowcell/){
+        next;
+      }
+      if(/^,+$/){
+        next;
+      }
+      if(/^\s*$/){
+        next;
+      }
+      print $fh $_;
+    }
+    close $demuxFh;
+  }
+  close $fh;
+
+  return $samplesheet;
+}
+
+
 
 sub usage{
   "Parses an unaltered Illumina run and formats it
@@ -152,5 +198,6 @@ sub usage{
   
   --numcpus  1
   --outdir   ''
+  --createsamplesheet
   "
 }
