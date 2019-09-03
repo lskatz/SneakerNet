95dcc21c4f9deae74d864f87d634b896ee88d2fa
edlb-sneakernet <edlb-sneakernet@scicomp>
2019-09-03 16:17:00 -0400

    hello world in python

--- SneakerNet.plugins/sn_helloWorld.py
+++ SneakerNet.plugins/sn_helloWorld.py
@@ -2,112 +2,94 @@
 # Hello World example
 
 import tempfile
-import argparse
+import click
 import sys
 import os
+import csv
 
 VERSION='1.0'
 CITATION='Hello World example by Lee Katz'
 
-def main(args):
-  print(args);
+@click.command()
+@click.option("-v", "--version",  is_flag=True, default=False, help="show the version and exit")
+@click.option("-c", "--citation", is_flag=True, default=False, help="show the citation and exit")
+@click.option("-d", "--debug",    is_flag=True, default=False, help="show debugging information")
+@click.option("-f", "--force",    is_flag=True, default=False, help="Force output")
+@click.option("-t", "--tempdir", default="", type=str, help="Define where the temporary directory is")
+@click.option("-n", "--numcpus", nargs=1, default=1,  type=int, help="Number of cpus")
+@click.argument("dir", nargs=1, default="")
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="SneakerNet Options", allow_abbrev=True)
-  parser.add_argument('-v','--version',  action='version', version=VERSION);
-  parser.add_argument('-c','--citation', action='store_true', help="Show the citation and exit");
-  parser.add_argument('-t','--tempdir',  nargs=1, type=str, help="Set a temporary directory path. Default: tempfile.TemporaryDirectory()")
-  args = parser.parse_args();
-  if args.citation:
-    print(CITATION)
-    sys.exit(0)
-    
+def main(dir,version, citation, force, debug, tempdir, numcpus):
+  # Take care of version, citation
+  setup(version, citation)
+
+  if not os.path.isdir(dir):
+    print("ERROR: could not find directory "+dir)
+    sys.exit(1)
+
   with tempfile.TemporaryDirectory() as tempdirname:
-    if args.tempdir is None:
-      args.tempdir = tempdirname
+    # If not defined already, create a new tempdir
+    if not tempdir:
+      tempdir = tempdirname
+
+    # If the user defined the new temporary directory, then
+    # just accept it.
     else:
-      args.tempdir = args.tempdir[0];
-      print(args.tempdir)
-      if os.path.isdir(args.tempdir):
-        print("ERROR: "+args.tempdir+" already exists!")
+      if os.path.isdir(tempdir):
+        print("ERROR: "+tempdir+" already exists!")
         sys.exit(1)
       else:
-        os.mkdir(args.tempdir)
-
-    main(args)
-
-
-"""
-use strict;
-use warnings;
-use Getopt::Long;
-use Data::Dumper;
-use File::Basename qw/fileparse basename dirname/;
-use File::Temp qw/tempdir/;
-use File::Copy qw/mv cp/;
-use Bio::SeqIO;
-use Bio::FeatureIO::gff;
-
-use FindBin;
-use lib "$FindBin::RealBin/../lib/perl5";
-use SneakerNet qw/recordProperties readConfig samplesheetInfo_tsv command logmsg fullPathToExec/;
-
-our $VERSION = "1.0";
-
-local $0=fileparse $0;
-exit(main());
-
-sub main{
-  my $settings=readConfig();
-  GetOptions($settings,qw(version help force tempdir=s debug numcpus=i)) or die $!;
-  if($$settings{version}){
-    print $VERSION."\n";
-    return 0;
-  }
-
-  die usage() if($$settings{help} || !@ARGV);
-  $$settings{numcpus}||=1;
-  $$settings{tempdir}||=File::Temp::tempdir(basename($0).".XXXXXX",TMPDIR=>1,CLEANUP=>1);
-
-  my $dir=$ARGV[0];
-
-  my $outfile = makeTable($dir, $settings);
-  logmsg "Table can be found at $outfile";
-
-  recordProperties($dir,{version=>$VERSION, table=>$outfile});
-
-  return 0;
-}
-
-sub makeTable{
-  my($dir, $settings)=@_;
-
-  my $outfile = "$dir/SneakerNet/forEmail/helloWorld.tsv";
-  open(my $fh, ">", $outfile) or die "ERROR writing to $outfile: $!";
-  print $fh join("\t",qw(key value))."\n";
-  for my $key(qw(version help force tempdir debug numcpus)){
-    $$settings{$key} ||= 0;
-    print $fh join("\t", $key, $$settings{$key})."\n";
-  }
-  close $fh;
-
-  return $outfile;
-}
+        os.mkdir(tempdir)
     
 
-sub usage{
-  "Run Hello World as a plugin for SneakerNet.
-  Creates a table in the 'forEmail' folder with which
-  options have been specified.
+    # make the directory structure just in case it isn't there yet
+    for subdir in dir + "/SneakerNet", dir + "/SneakerNet/forEmail":
+      if not os.path.isdir(subdir):
+        os.mkdir(subdir)
 
-  Usage: $0 MiSeq_run_dir
-  --numcpus  1  Number of CPUs
-  --force       Whether to overwrite results
-  --debug       Print debugging information
-  --version     Print the version and exit
-  --help        This usage menu
-  --tempdir  '' Specify the temporary directory
-  "
-}
-"""
+    # analysis
+    samples = readWriteSamples(dir)
+    writeProperties(dir, samples)
+
+    return 0
+
+  return 1
+
+def writeProperties(dir, samples):
+  propertiesFile = dir+"/SneakerNet/properties.txt"
+  with open(propertiesFile, 'ta+') as fileout:
+    fileout.write("{}\t{}\t{}\n".format(
+      "sn_helloWorld.py", "version", VERSION
+    ))
+    fileout.write("{}\t{}\t{}\n".format(
+      "sn_helloWorld.py", "table", samples
+    ))
+
+def readWriteSamples(dir):
+  samplesout = dir + "/SneakerNet/forEmail/helloworld.py.tsv"
+  samples = dir + "/samples.tsv"
+  with open(samples, 'rt') as tsvin, open(samplesout,'wt') as tsvout:
+    tsvin = csv.reader(tsvin, delimiter='\t')
+    #tsvout= csv.writer(tsvout,delimiter='\t')
+
+    tsvout.write("{}\t{}\n".format("sample","sampleCounter"))
+    counter = 0
+    for row in tsvin:
+      counter += 1
+      tsvout.write("{}\t{}\n".format(row[0],counter))
+      #tsvout.writerow(row[0])
+
+  return samplesout
+
+def setup(version, citation):
+  if citation:
+    print(CITATION)
+    sys.exit(0)
+  if version:
+    print(VERSION)
+    sys.exit(0)
+
+if __name__ == "__main__":
+  # Main program
+  sys.exit(main())
 
