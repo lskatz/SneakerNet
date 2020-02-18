928dbd946c59a9f83b5bac33cda0a72bd3564191
Lee Katz - Aspen <gzu2@cdc.gov>
2020-02-18 15:54:47 -0500

    gp60 counter

--- SneakerNet.plugins/sn_crypto_gp60.pl
+++ SneakerNet.plugins/sn_crypto_gp60.pl
@@ -6,7 +6,7 @@ use warnings;
 use Getopt::Long;
 use Data::Dumper;
 use File::Basename qw/fileparse basename dirname/;
-use File::Temp qw/tempdir/;
+use File::Temp qw/tempdir tempfile/;
 use File::Copy qw/mv cp/;
 use Bio::SeqIO;
 use Bio::FeatureIO::gff;
@@ -46,11 +46,27 @@ sub main{
   mkdir "$dir/SneakerNet";
   mkdir "$dir/SneakerNet/gp60";
   mkdir "$dir/SneakerNet/forEmail";
-  my $reportArr=gp60ForAll($dir,$settings);
+
+  my $db = $$settings{gp60db} || die "ERROR: need to add gp60db to settings.conf";
+
+  my $reportHash = gp60ForAll($dir,$db,$settings);
   my $reportFile = "$dir/SneakerNet/forEmail/gp60.tsv";
 
   # Make a report for email
-  die;
+  open(my $outFh, '>', $reportFile) or die "ERROR: could not write to $reportFile: $!";
+  print $outFh join("\t", qw(sample gp60))."\n";
+  while(my($sample, $report) = each(%$reportHash)){
+    open(my $inFh, '<', $report) or die "ERROR: could not read from $report: $!";
+    my $result = <$inFh>;
+    close $inFh;
+    chomp($result);
+    my @F = split(/\t/, $result);
+
+    print $outFh join("\t", $sample, $F[3])."\n";
+  }
+  close $outFh;
+
+  logmsg "Results found in $reportFile";
 
   recordProperties($dir,{version=>$VERSION, table=>$reportFile});
 
@@ -58,7 +74,7 @@ sub main{
 }
 
 sub gp60ForAll{
-  my($dir, $settings) = @_;
+  my($dir, $db, $settings) = @_;
 
   my @queueBuffer = ();
 
@@ -77,22 +93,54 @@ sub gp60ForAll{
     push(@queueBuffer, {assembly=>$assembly, sample=>$sample});
   }
 
+  # make sure that the gp60 database has been formatted
+  for my $dbFile("$db.nhr", "$db.nin", "$db.nsq"){
+    if(! -e $dbFile){
+      logmsg "WARNING: database was given as $db but it might not be formatted. Could not find file $dbFile";
+      logmsg "WARNING: formatting the database myself.";
+      command("makeblastdb -in $db -dbtype nucl");
+      last;
+      #die "ERROR: database was given as $db but it might not be formatted. Could not find file $dbFile";
+    }
+  }
+
   # Kick off the threads with the array buffer
   my $Q=Thread::Queue->new(@queueBuffer);
   my @thr;
   for(my $i=0;$i<$$settings{numcpus};$i++){
-    $thr[$i]=threads->new(\&gp60Worker,$Q,$dir,$settings);
+    $thr[$i]=threads->new(\&gp60Worker,$Q,$dir,$db,$settings);
     # also send a terminating signal
     $Q->enqueue(undef);
   }
 
-  my @reportArr=();
+  my %reportHash =();
   for(@thr){
-    my $reportList=$_->join;
-    push(@reportArr, @$reportList);
+    my $reports = $_->join;
+    %reportHash = (%reportHash, %$reports);
   }
 
-  return \@reportArr;
+  return \%reportHash;
+}
+
+sub gp60Worker{
+  my($Q, $dir, $db, $settings) = @_;
+
+  my %report;
+
+  while(defined(my $info = $Q->dequeue)){
+    my $asm    = $$info{assembly};
+    my $sample = $$info{sample};
+    my($blastoutFh, $blastout) = tempfile("XXXXXX", SUFFIX=>".bls.tsv",  DIR=>$$settings{tempdir}, CLEANUP=>1);
+    my($gp60Fh, $gp60)         = tempfile("XXXXXX", SUFFIX=>".gp60.tsv", DIR=>$$settings{tempdir}, CLEANUP=>1);
+    
+    command("blastn -query $asm -db $db -outfmt '6 qseqid sseqid pident length qcovhsp mismatch gapopen qstart qend sstart send qlen slen qseq' | head -n 1 > $blastout");
+
+    command("countGP60repeats.pl $blastout > $gp60");
+    
+    $report{$sample} = $gp60;
+  }
+
+  return \%report;
 }
 
 sub usage{
