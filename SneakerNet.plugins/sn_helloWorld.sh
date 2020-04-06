ee6132fcc75930b3490e0b346e64c7c50c59b43c
Lee Katz - Aspen <gzu2@cdc.gov>
2020-04-06 13:53:20 -0400

    Updated Bash Hello World for Bash, to help conform to plugin guidelines

--- SneakerNet.plugins/sn_helloWorld.sh
+++ SneakerNet.plugins/sn_helloWorld.sh
@@ -1,71 +1,147 @@
 #!/bin/bash
 
-function usage(){
-  echo "Usage: $0 [options] run/"
-  echo "options"
-  echo "  -h, -d, -t, -f, -v, -c"
-  echo "  -n 1"
-}
+set -e
+set -u
 
 # Set defaults
-NUMCPUS=1
 SCRIPT=$(basename $0)
-VERSION=1
+MY_VERSION=2
+MY_CITATION="Hello World example in Bash by Lee Katz"
+
+function usage(){
+  echo "Usage: $0 [options] run/"
+  echo "Prints all options to run/SneakerNet/properties.txt"
+  echo "OPTIONS
+  --help
+  --numcpus 1
+  --debug
+  --tempdir ''
+  --force
+  --version
+  --citation
+  --check-dependencies
+  "
+}
 
 OPTIONSUSED=""
 
-while getopts "hdt:fvcn:" opt; do
-  case ${opt} in
-    h )
-      usage;
-      exit 0;
-      ;;
-    d )
-      echo "You provided '-d'"
-      OPTIONSUSED+=" d"
-      ;;
-    t )
-      echo "You provided '-t'"
-      TEMPDIR=$OPTARG
-      OPTIONSUSED+=" t=$TEMPDIR"
-      ;;
-    f )
-      echo "You provided '-f'"
-      OPTIONSUSED+=" f"
-      ;;
-    v )
-      echo "You provided '-v'"
-      OPTIONSUSED+=" v"
-      ;;
-    c )
-      echo "You provided '-c'"
-      OPTIONSUSED+=" c"
-      ;;
-    n )
-      NUMCPUS=$OPTARG
-      OPTIONSUSED+=" n=$NUMCPUS"
-      ;;
-    \? )
-      echo "Invalid option specified!"
-      exit 1
-    ;;
-  esac
+# Set expected variables and defaults to avoid "-u errors"
+# Booleans are ints in this example
+HELP=0
+NUMCPUS=1
+DEBUG=0
+TEMPDIR="" # TODO add mktemp
+FORCE=0
+VERSION=0
+CITATION=0
+CHECK_DEPENDENCIES=0
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --help|-h)
+            HELP=1
+            shift 1
+            ;;
+
+        --numcpus|-n)
+            NUMCPUS=$2
+            shift 2
+            ;;
+
+        --debug)
+            DEBUG=1
+            shift 1
+            ;;
+
+        --tempdir)
+            TEMPDIR=$2
+            shift 2
+            ;;
+
+        --force)
+            FORCE=1
+            shift 1
+            ;;
+
+        --version)
+            VERSION=1
+            shift 1
+            ;;
+
+        --citation)
+            CITATION=1
+            shift 1
+            ;;
+
+        --check-dependencies)
+            CHECK_DEPENDENCIES=1
+            shift 1
+            ;;
+
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
 done
-shift $((OPTIND -1))
 
-RUN=$1
-
-echo $RUN
-
-if [ -z $RUN ]; then
-  usage
-  exit 1
+# restore positional parameters if there are any
+if [[ "${#POSITIONAL[@]}" -gt 0 ]]; then 
+  set -- "${POSITIONAL[@]}"
 fi
 
-TABLE="$RUN/SneakerNet/forEmail/helloworld.sh.tsv"
-echo -e "foo\tbar" > $TABLE
+# exit if there are no arguments
+if [[ "$HELP" == 1 ]] || [[ $# -lt 1 ]]; then
+  echo "$HELP .. $#"; exit
+  usage;
+  exit 0;
+fi
 
-echo -e "$0\ttable\t$TABLE"     >> $RUN/SneakerNet/properties.txt
-echo -e "$0\tversion\t$VERSION" >> $RUN/SneakerNet/properties.txt
-echo -e "$0\toptions\t$OPTIONSUSED" >> $RUN/SneakerNet/properties.txt
+# print and exit for certain options
+if [[ "$CITATION" == 1 ]]; then
+  # Print the citation and exit
+  echo "$MY_CITATION"
+  exit 0;
+fi
+if [[ "$VERSION" == 1 ]]; then
+  # Print the version and exit
+  echo "$MY_VERSION"
+  exit 0;
+fi
+if [[ "$CHECK_DEPENDENCIES" == 1 ]]; then
+  # Check for dependencies: print results to stderr and execs checked to stdout
+
+  # Check version of bash
+  echo "bash"
+  bash --version 2>&1 | grep -m 1 version 1>&2
+
+  # Check version of basename
+  echo "basename"
+  basename --version 2>&1 | grep -m 1 basename 1>&2
+
+  exit 0;
+fi
+
+# The run number is the first positional argument
+RUN=$1
+
+echo "Run was given as $RUN"
+
+# Write all options to a table
+TABLE="$RUN/SneakerNet/forEmail/helloworld.sh.txt"
+echo -e "foo\tbar"                                 > $TABLE
+echo -e "run\t$RUN"                               >> $TABLE
+echo -e "help\t$HELP"                             >> $TABLE
+echo -e "numcpus\t$NUMCPUS"                       >> $TABLE
+echo -e "debug\t$DEBUG"                           >> $TABLE
+echo -e "tempdir\t$TEMPDIR"                       >> $TABLE
+echo -e "force\t$FORCE"                           >> $TABLE
+echo -e "VERSION\t$VERSION"                       >> $TABLE
+echo -e "CITATION\t$CITATION"                     >> $TABLE
+echo -e "check-dependencies\t$CHECK_DEPENDENCIES" >> $TABLE
+echo "Wrote to table $TABLE"
+
+# Record properties from this plugin into properties.txt
+echo -e "$SCRIPT\ttable\t$TABLE"        >> $RUN/SneakerNet/properties.txt
+echo -e "$SCRIPT\tversion\t$MY_VERSION" >> $RUN/SneakerNet/properties.txt
 
